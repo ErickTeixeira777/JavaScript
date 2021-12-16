22c9b1a10fa4e569a616acd985c35235e92f498f
Erick Teixeira <contatoerickvenon@gmail.com>
2021-12-15 21:21:33 -0300

    update

---
+++ Ambiente/ambiente.js
@@ -0,0 +1,13 @@
+/*Laço Simples
+Estrutura de repetição
+-----------------------
+var c = 1 
+while(c <= 7){
+    console.log('Code Save')
+}
+*/
+var c = 1
+  do {
+    console.log('Code Save')
+    c++
+} while(c <= 7)
---
+++ Ex09/ex009.js
@@ -0,0 +1,10 @@
+var país = 'Espanha'
+console.log(`Vivendo na ${país}`)
+if (país != 'Espanha'){
+    console.log('Você é Estrangeiro')
+}   else {
+      console.log('Você é Espanhol')
+}
+
+
+    
