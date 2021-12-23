269b6c165b48295a11a3bdb6ec8121eb8adab4fa
Erick Teixeira <contatoerickvenon@gmail.com>
2021-12-23 19:28:18 -0300

    up

--- Ex13/ex013.js
+++ Ex13/ex013.js
@@ -1,8 +1,22 @@
 function carregar() {
-    var hora = window.document.getElementById('msg')
+    var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
-    var date = new Date()
+    var data = new Date()
     var hora = data.getHours()
-    var minuto = data.getMinutes()
+    
     msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+
+        img.src = 'manha.png'
+        msg.innerHTML += 'BOM DIA!'
+        
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'tarde.png'
+        msg.innerHTML += 'BOA TARDE!'
+       
+    }else{
+       img.src = 'noite.png'
+       msg.innerHTML += 'BOA NOITE!'
+   
+    }
 }
