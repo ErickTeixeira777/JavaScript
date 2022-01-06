eeea385e83256eaa7db86173ad2dda3aeaef58b1
Erick Teixeira <contatoerickvenon@gmail.com>
2022-01-05 21:32:15 -0300

    up date

--- Ex15/script.js
+++ Ex15/script.js
@@ -8,7 +8,35 @@ function verificar () {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        var gênero = ''
+        var img =document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+            img.setAttribute('src','bebemasc.png')
+            } else if (idade < 21) {
+            img.setAttribute('src','jovemmasc.png')
+            } else if (idade < 50) {
+            img.setAttribute('src','adulto.png')    
+            } else {
+            img.setAttribute('src','senhor.png')    
+            }
+        }else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+            img.setAttribute('src','bebefemi.png')
+            } else if (idade < 21) {
+            img.setAttribute('src','jovemfemi.png')
+            } else if (idade < 50) {
+            img.setAttribute('src','adulta.png')    
+            } else {
+            img.setAttribute('src','senhora.png') 
+            }
+        }   
+        res.style.textAling = 'center'
+        res.innerHTML = `${gênero} com ${idade} anos`
+        res.appendChild(img)
     }
 
 }
